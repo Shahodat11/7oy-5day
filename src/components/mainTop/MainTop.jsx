794a3d706cbbd5dabfe9a1c82545e8e1aba95ae0
unknown <shahodatusmonjonova@gmail.com>
2024-05-16 08:47:45 +0500

    “izoh”

--- src/components/mainTop/MainTop.jsx
+++ src/components/mainTop/MainTop.jsx
@@ -11,13 +11,14 @@ function MainTop() {
             <img className='tizig' src={tizig} alt="" />
             <ul>
                 <li>
-                    <a href="#">about us</a>
+                    <a href="#" className='a'>about us</a>
                 </li>
             </ul>
-            <p>If you’re looking for a Premium Quality
+            <p className='p'>If you’re looking for a Premium Quality
                Tempered Glass or Back-cover for
                your Device </p>
-            <img src={img} alt="" />
+            <p className='lorem'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+            <img className='banner' src={img} alt="" />
         </div>
         <div className="nav-link2">
             <div className="navv">
