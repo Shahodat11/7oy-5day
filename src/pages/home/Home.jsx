f3e3cd9c0d6f0604a038d95978a5f9b743a3e550
unknown <shahodatusmonjonova@gmail.com>
2024-05-16 10:58:16 +0500

    davomi

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,8 @@ import Main from "../../components/main/Main"
 import Products from '../../components/products/Products'
 import { useGetProductsQuery } from '../../context/productApi'
 import MainTop from '../../components/mainTop/MainTop'
+import MainEnd from '../../components/mainEnd/MainEnd'
+import Footer from '../../components/footer/Footer'
 
 const Home = () => {
   let {data, isLoading, error, isError} = useGetProductsQuery()
@@ -12,6 +14,8 @@ const Home = () => {
       <Main/>
       <Products data={data?.products}/>
       <MainTop/>
+      <MainEnd/>
+      <Footer/>
     </div>
   )
 }
